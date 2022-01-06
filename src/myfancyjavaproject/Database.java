2ce5fee208efe20a89dc6c648327fe0b85280c27
Safiul Bari <asmsafiulbari@gmail.com>
2022-01-06 17:06:56 +0600

    first commit

---
+++ src/myfancyjavaproject/Database.java
@@ -0,0 +1,7 @@
+package myfancyjavaproject;
+
+class Database {
+   public void getConnection() {
+       System.out.println("You are now connected to the database.");
+   }
+}
---
+++ src/myfancyjavaproject/Main.java
@@ -0,0 +1,8 @@
+package myfancyjavaproject;
+
+class Main {
+   public static void main(String[] args) {
+      Database db1 = new Database();
+      db1.getConnection();
+   }
+}
