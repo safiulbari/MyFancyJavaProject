29223cd85890ba84ad78f2b3e0a075638c1528d2
sifatuiu <m.sifat1997@gmail.com>
2022-01-09 00:02:30 +0600

    implemented singleton pattern

--- src/myfancyjavaproject/Database.java
+++ src/myfancyjavaproject/Database.java
@@ -1,7 +1,19 @@
 package myfancyjavaproject;
 
 class Database {
-   public void getConnection() {
-       System.out.println("You are now connected to the database.");
-   }
+    private static Database dbObject;
+
+    private Database() {      
+    }
+
+    public static Database getInstance() {
+       if(dbObject == null) {
+          dbObject = new Database();
+       }
+        return dbObject;
+    }
+ 
+    public void getConnection() {
+        System.out.println("You are now connected to the database.");
+    }
 }
