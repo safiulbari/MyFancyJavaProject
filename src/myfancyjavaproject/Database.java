1173b1c110efaa53fc46905f1ebb57951e9d3947
safiulbari <asmsafiulbari@gmail.com>
2022-01-10 22:55:56 +0600

    slightly changed in code by X

--- src/myfancyjavaproject/Database.java
+++ src/myfancyjavaproject/Database.java
@@ -3,5 +3,6 @@ package myfancyjavaproject;
 class Database {
    public void getConnection() {
        System.out.println("You are now connected to the database.");
+       System.out.println("Database strating...");
    }
 }
